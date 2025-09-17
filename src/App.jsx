feee91e75035f2950c1e022be20d0e746c7a76a4
Ali Aafee <ali.aafee@gmail.com>
2025-09-17 13:24:31 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import { BotIcon } from "lucide-react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="overflow-hidden w-screen h-screen flex bg-radial-[at_50%_75%] from-sky-200 via-blue-400 to-indigo-900 to-90%">
+      <div className="m-auto rounded-2xl bg-black/50 p-4 w-80 text-center grid grid-cols-1 justify-items-center text-white">
+        <BotIcon width={64} height={64} />
+        <div>Hello Vite+React+Tailwind4+PocketBase</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
